ccde9658846a173f8694ec8fd05cace16f0b107d
ChaseRensberger <chasehrensberger@gmail.com>
2024-11-13 19:46:55 -0500

    begin work on fetching from horizon

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 // import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { Auth0Provider } from "@auth0/auth0-react";
+import { QueryProvider } from "@/lib/query";
 
 export const Route = createRootRoute({
   component: RootLayout,
@@ -8,17 +9,19 @@ export const Route = createRootRoute({
 
 function RootLayout() {
   return (
-    <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: `${window.location.origin}/browse`,
-      }}
-      cacheLocation="localstorage"
-      useRefreshTokens={true}
-    >
-      <Outlet />
-      {/* <TanStackRouterDevtools /> */}
-    </Auth0Provider>
+    <QueryProvider>
+      <Auth0Provider
+        domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: `${window.location.origin}/browse`,
+        }}
+        cacheLocation="localstorage"
+        useRefreshTokens={true}
+      >
+        <Outlet />
+        {/* <TanStackRouterDevtools /> */}
+      </Auth0Provider>
+    </QueryProvider>
   );
 }
